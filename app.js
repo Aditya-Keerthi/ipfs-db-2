9acb3e8d4ce9afc04749c90d800b511b18059dd9
Aditya-Keerthi <54603000+Aditya-Keerthi@users.noreply.github.com>
2020-05-03 12:34:48 -0400

    hi

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+// var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var createMasterDB = require('./routes/createMasterDB');
 var query = require('./routes/query');
@@ -33,6 +33,9 @@ app.use('/updateValues', updateValues)
 app.use('/createMasterDB', createMasterDB)
 app.use('/getDB', getDB);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/iApp.js'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
